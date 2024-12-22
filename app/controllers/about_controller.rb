95d1b415ad98fb786899e6e1ccc3578f16883cbe
shubhamiitr <shubham.kansal@apollo.io>
2024-12-22 21:59:20 +0530

    Adding bootstrap library, added root and about routes

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+    # Add any logic you need for the about page here
+  end
+end
---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def index
+    # Your code for the index action
+  end
+end
